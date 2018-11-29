f13a42644ee3a12adcb90a65c466d4bc4e6743f9
Tamas Schneider <snaaajder@gmail.com>
2018-11-29 23:05:35 +0000

    Add buttons to header

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>Header placeholder</div>
+            <div className="btn-group d-flex" role="group">
+                <button type="button" className="btn btn-default w-100">Settlements</button>
+                <button type="button" className="btn btn-default w-100">Chart</button>
+            </div>
         );
     }
 }
