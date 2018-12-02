1408178ee097bb99659c2c848201f2a6aa851769
Tamas Schneider <snaaajder@gmail.com>
2018-12-02 22:51:08 +0000

    Fix navbar style

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,7 @@ class NavBar extends Component {
         return (
             <nav className="navbar navbar-expand navbar-dark fixed-top bg-dark py-0">
                 <ul className="navbar-nav w-100 nav-justified">
-                    <li className="nav-item d-flex flex-row">
+                    <li className="nav-item">
                         <a className="nav-link" href="#">Settlements</a>
                     </li>
                     <li className="nav-item">
