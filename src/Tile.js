92d1beef0a8944c1592ee3ff7f33cab753f0c178
Tamas Schneider <snaaajder@gmail.com>
2018-12-02 22:30:16 +0000

    Add number placeholder to Tile

--- src/Tile.js
+++ src/Tile.js
@@ -3,12 +3,15 @@ import React, {Component} from 'react';
 class Tile extends Component {
     render() {
         return (
-            <div>
+            <div className="tile">
                 <img
                     src="https://dummyimage.com/80x80/000/fff"
                     alt="Tile"
                     height="80"
                 />
+                <div className="tile-number">
+                    <span><b>8</b></span>
+                </div>
             </div>
         );
     }
