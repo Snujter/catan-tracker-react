91d2496c1d7b9b164a0085cd2695da5f545ed372
Tamas Schneider <snaaajder@gmail.com>
2018-12-02 22:22:54 +0000

    Add layout to settlement

--- src/Settlement.js
+++ src/Settlement.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
+import Tile from './Tile';
 
 class Settlement extends Component {
     render() {
         return (
             <div>
-                settlement placeholder
+                <img src={require("./images/settlement.png")} alt="Settlement" height="60"/>
+
+                <div className="d-flex justify-content-center bd-highlight mb-3">
+                    <div className="p-2 bd-highlight">
+                        <Tile/>
+                    </div>
+                    <div className="p-2 bd-highlight">
+                        <Tile/>
+                    </div>
+                    <div className="p-2 bd-highlight">
+                        <Tile/>
+                    </div>
+                </div>
             </div>
         );
     }
