beb56d63f4b91eae19767080767f4c4b0c26257d
Tamas Schneider <snaaajder@gmail.com>
2018-12-03 18:22:19 +0000

    Add prop to see if settlement is city

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
         const settlements = this.state.settlements.slice();
         this.setState({
             settlements: settlements.concat({
+                isCity: false,
             }),
         });
     }
