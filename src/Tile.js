635040bacae98e6dcf80d99677a57e4900304d89
Tamas Schneider <snaaajder@gmail.com>
2018-12-03 13:55:01 +0000

    Improve Tile to use pictures

--- src/Tile.js
+++ src/Tile.js
@@ -1,19 +1,42 @@
 import React, {Component} from 'react';
+import brickHex from './images/brick-hex.png';
+import forestHex from './images/forest-hex.png';
+import fieldHex from './images/field-hex.png';
+import rockHex from './images/rock-hex.png';
+import wheatHex from './images/wheat-hex.png';
+import emptyHex from './images/empty-hex.png';
 
 class Tile extends Component {
     render() {
         return (
-            <div className="tile">
+            <div className="tile" onClick={this.props.onClick}>
                 <img
-                    src="https://dummyimage.com/80x80/000/fff"
+                    src={this.getImage()}
                     alt="Tile"
                     height="90"
                 />
                 <div className="tile-number">
-                    <span><b>8</b></span>
+                    <span><b>{this.props.number}</b></span>
                 </div>
             </div>
         );
+    }
+
+    getImage() {
+        switch (this.props.type) {
+            case 'brick':
+                return brickHex;
+            case 'forest':
+                return forestHex;
+            case 'field':
+                return fieldHex;
+            case 'rock':
+                return rockHex;
+            case 'wheat':
+                return wheatHex;
+            default:
+                return emptyHex;
+        }
     }
 }
 
