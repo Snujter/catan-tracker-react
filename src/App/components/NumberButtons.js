7b13eb38c4d641e67820ff072b90339a5c09e42d
Tamas Schneider <snaaajder@gmail.com>
2018-12-05 19:56:29 +0000

    Add optional disabled numbers to NumberButton

--- src/App/components/NumberButtons.js
+++ src/App/components/NumberButtons.js
@@ -4,21 +4,30 @@ import { availableRolls } from '../../immutables';
 
 class NumberButtons extends Component {
     render() {
-        const { numbers, selectedNumber, onButtonClick, className } = this.props;
+        const { numbers, selectedNumber, onButtonClick, className, disabledNumbers } = this.props;
 
         return (
             <div className={className}>
                 {(numbers || availableRolls).map((number, i) => {
-                    const extraClasses = (number === selectedNumber) ?
-                        "bg-dark text-light" :
-                        "bg-light text-dark";
+                    const isDisabled = disabledNumbers
+                        && disabledNumbers.length
+                        && disabledNumbers.indexOf(number) !== -1;
+
+                    let extraClasses;
+                    if (isDisabled) {
+                        extraClasses = "disabled";
+                    } else if (number === selectedNumber) {
+                        extraClasses = "bg-dark text-light";
+                    } else {
+                        extraClasses = "bg-light text-dark";
+                    }
 
                     return (
                         <NumberButton
                             key={i}
                             number={number}
                             extraClasses={extraClasses}
-                            onClick={() => onButtonClick(number)}
+                            onClick={() => onButtonClick && onButtonClick(number)}
                         />
                     );
                 })}
