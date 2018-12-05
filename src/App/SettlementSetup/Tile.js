2377b33b404fefd116b4826b89ceaa7e94b75a41
Tamas Schneider <snaaajder@gmail.com>
2018-12-05 10:46:01 +0000

    Make tile number optional

--- src/App/SettlementSetup/Tile.js
+++ src/App/SettlementSetup/Tile.js
@@ -4,13 +4,19 @@ import TileImage from '../components/TileImage';
 class Tile extends Component {
     render() {
         const { type, number, onClick } = this.props;
+        let numberDisplay;
+        if (number) {
+            numberDisplay = (
+                <div className="tile-number">
+                    <span><b>{number}</b></span>
+                </div>
+            );
+        }
 
         return (
             <div className="tile" onClick={onClick}>
                 <TileImage type={type}/>
-                <div className="tile-number">
-                    <span><b>{number}</b></span>
-                </div>
+                {numberDisplay}
             </div>
         );
     }
