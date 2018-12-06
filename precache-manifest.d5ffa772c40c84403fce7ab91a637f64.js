2b1b3a7cc58b3fda64a8161a39fe941ee80528fd
Tamas Schneider <snaaajder@gmail.com>
2018-12-06 20:02:59 +0000

    Updates

---
+++ precache-manifest.d5ffa772c40c84403fce7ab91a637f64.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c7735162dfa66c7f6c99ed175d2e362",
+    "url": "/catan-tracker-react/static/media/field-hex.8c773516.png"
+  },
+  {
+    "revision": "fd5b5a34ed35974c9014",
+    "url": "/catan-tracker-react/static/css/main.effcd1dc.chunk.css"
+  },
+  {
+    "revision": "26af67f7c448c99f237d6b5ed3f9d4be",
+    "url": "/catan-tracker-react/static/media/city.26af67f7.png"
+  },
+  {
+    "revision": "9f097d15ebde57afe866",
+    "url": "/catan-tracker-react/static/js/1.9f097d15.chunk.js"
+  },
+  {
+    "revision": "a0faadb6bc2ada00dc9c",
+    "url": "/catan-tracker-react/static/js/runtime~main.a0faadb6.js"
+  },
+  {
+    "revision": "fab879f8773d7cea9c0bed2754cdd76c",
+    "url": "/catan-tracker-react/static/media/brick-hex.fab879f8.png"
+  },
+  {
+    "revision": "8725920dbc3e526facecb40265dd2069",
+    "url": "/catan-tracker-react/static/media/forest-hex.8725920d.png"
+  },
+  {
+    "revision": "fd5b5a34ed35974c9014",
+    "url": "/catan-tracker-react/static/js/main.fd5b5a34.chunk.js"
+  },
+  {
+    "revision": "a45b3e02c794c3ff8b8e41c4b0221c16",
+    "url": "/catan-tracker-react/static/media/rock-hex.a45b3e02.png"
+  },
+  {
+    "revision": "dcd53bed665a3bdf202278acca61a3ba",
+    "url": "/catan-tracker-react/static/media/wheat-hex.dcd53bed.png"
+  },
+  {
+    "revision": "f50bee80e24c380596bfa12ca9c6877a",
+    "url": "/catan-tracker-react/static/media/empty-hex.f50bee80.png"
+  },
+  {
+    "revision": "694f53ee2c4d2344bc7fdba958d2f3df",
+    "url": "/catan-tracker-react/static/media/settlement.694f53ee.png"
+  },
+  {
+    "revision": "9f097d15ebde57afe866",
+    "url": "/catan-tracker-react/static/css/1.a3619919.chunk.css"
+  },
+  {
+    "revision": "4d352a3307db2fd10bed3e7381502fe7",
+    "url": "/catan-tracker-react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/catan-tracker-react/precache-manifest.d5ffa772c40c84403fce7ab91a637f64.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/catan-tracker-react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
